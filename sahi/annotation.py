57b2632b42a88b227ae8247a497e57ee13bb7410
aure <85502331+aureguerrero@users.noreply.github.com>
2022-02-19 14:08:32 -0300

    Update annotation.py

--- sahi/annotation.py
+++ sahi/annotation.py
@@ -247,7 +247,8 @@ class Mask:
         
         ##$$##
         punto=np.min(np.where(self.bool_mask == True),axis=1)
-        starting_pixel = [self.shift_x+punto[1], self.shift_y+punto[0]]
+        #starting_pixel = [self.shift_x+punto[1], self.shift_y+punto[0]]
+        starting_pixel = [self.shift_x,self.shift_y]
         ending_pixel = [
             min(starting_pixel[0] + self.bool_mask.shape[1], self.full_shape_width),
             min(starting_pixel[1] + self.bool_mask.shape[0], self.full_shape_height),
@@ -259,8 +260,8 @@ class Mask:
         ]
 ###$$##
         return Mask(
-            mask_fullsized,
-            shift_amount=[punto[1],punto[0]],
+            mask_fullsized[starting_pixel[1]+punto[1] : ending_pixel[1], starting_pixel[0]+punto[0] : ending_pixel[0]],
+            shift_amount=[self.shift_x+punto[1], self.shift_y+punto[0]],
             full_shape=self.full_shape,
         )
 
@@ -539,7 +540,7 @@ class ObjectAnnotation:
             if bbox_from_bool_mask is not None:
                 bbox = bbox_from_bool_mask
                 caja=[np.min(np.where(bool_mask == True),axis=1),np.max(np.where(bool_mask == True),axis=1)]
-                self.mask.bool_mask=bool_mask[caja[0][0]:caja[1][0],caja[0][1]:caja[1][1]]
+                self.mask.bool_mask=bool_mask[:caja[1][0],:caja[1][1]]
             else:
                 raise ValueError("Invalid boolean mask.")
         else:
