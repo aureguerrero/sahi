e67379d6a612a012120f9ab3be179cd2fd0e1710
aure <85502331+aureguerrero@users.noreply.github.com>
2022-02-19 15:02:10 -0300

    Update annotation.py

--- sahi/annotation.py
+++ sahi/annotation.py
@@ -260,7 +260,7 @@ class Mask:
         ]
 ###$$##
         return Mask(
-            mask_fullsized[starting_pixel[1]+punto[1] : ending_pixel[1], starting_pixel[0]+punto[0] : ending_pixel[0]],
+            mask_fullsized[starting_pixel[1]+punto[0] : ending_pixel[1], starting_pixel[0]+punto[1] : ending_pixel[0]],
             shift_amount=[self.shift_x+punto[1], self.shift_y+punto[0]],
             full_shape=self.full_shape,
         )
