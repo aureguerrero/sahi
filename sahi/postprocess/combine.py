308f82bed8139c799a226692bfa158c0c51a1140
aure <85502331+aureguerrero@users.noreply.github.com>
2022-02-24 01:28:53 -0300

    Update combine.py

--- sahi/postprocess/combine.py
+++ sahi/postprocess/combine.py
@@ -563,9 +563,9 @@ class GreedyNMMPostprocess(PostprocessPredictions):
                     contours, hier = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                     cont=np.array(contours).squeeze()
                     if len(np.shape(cont))==1:
-                        areas=np.aaray([cv2.contourArea(cont[i]) for i in range(len(np.shape(cont))+1)])
+                        areas=np.array([cv2.contourArea(cont[i]) for i in range(len(np.shape(cont))+1)])
                         sacar=np.where(areas != np.max(areas))
-                        for i in sacar:
+                        for i in sacar[0]:
                             img=cv2.fillConvexPoly(img,cont[i],(0))
                         object_prediction_list[merge_ind].tolist().mask.bool_mask=np.array(img,dtype='bool')
                     selected_object_predictions.append(object_prediction_list[merge_ind].tolist())
