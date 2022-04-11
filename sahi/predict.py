f71aef2cc71a80f2764eb887af290bdc595918d7
aure <85502331+aureguerrero@users.noreply.github.com>
2022-04-11 20:50:50 -0300

    Update predict.py

--- sahi/predict.py
+++ sahi/predict.py
@@ -360,8 +360,8 @@ def get_sliced_prediction(
         while cant_old!=cant_new and i<=2:
             cant_old=len(object_prediction_list2)
             object_prediction_list2 = postprocess(object_prediction_list)
-            if perform_standard_pred:
-                postprocess.match_threshold=postprocess.match_threshold-min([max([0,(1-postprocess.match_threshold)/2]),0.1])
+            ##if perform_standard_pred:
+            ##    postprocess.match_threshold=postprocess.match_threshold-min([max([0,(1-postprocess.match_threshold)/2]),0.1])
             postprocess.match_metric="IOS"
             cant_new=len(object_prediction_list2)
             object_prediction_list=object_prediction_list2.copy()
