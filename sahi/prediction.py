cc6c176bcc7619034a4b1eee480ea70aa40207a7
aure <85502331+aureguerrero@users.noreply.github.com>
2022-05-20 21:16:56 -0300

    Update prediction.py

--- sahi/prediction.py
+++ sahi/prediction.py
@@ -251,7 +251,7 @@ class PredictionResult:
                     u=[p for p in range(len(self.object_prediction_list)) if p not in ubica[ubica2[0]]]
                     self.centroides=[self.centroides[t] for t in u]
                     centros=centros[u]
-                    self.object_prediction_list=[self.object_prediction_list[t] for t in u]
+#                     self.object_prediction_list=[self.object_prediction_list[t] for t in u]
                     object_prediction_list.extend([self.object_prediction_list[t] for t in u])
 
     #             lineas_d_surcos.append(np.poly1d([huber.coef_[0],huber.intercept_]))
@@ -277,7 +277,7 @@ class PredictionResult:
                     u=[p for p in range(len(self.object_prediction_list)) if p not in ubica[ubica2[0]]]
                     self.centroides=[self.centroides[t] for t in u]
                     centros=centros[u]
-                    self.object_prediction_list=[self.object_prediction_list[t] for t in u]
+#                     self.object_prediction_list=[self.object_prediction_list[t] for t in u]
                     object_prediction_list.extend([self.object_prediction_list[t] for t in u])
 
     #             lineas_d_surcos.append(np.poly1d([huber.coef_[0],huber.intercept_]))
@@ -302,7 +302,7 @@ class PredictionResult:
                     u=[p for p in range(len(self.object_prediction_list)) if p not in ubica[ubica2[0]]]
                     self.centroides=[self.centroides[t] for t in u]
                     centros=centros[u]
-                    self.object_prediction_list=[self.object_prediction_list[t] for t in u]
+#                     self.object_prediction_list=[self.object_prediction_list[t] for t in u]
                     object_prediction_list.extend([self.object_prediction_list[t] for t in u])
 
     #             lineas_d_surcos.append(np.poly1d([huber.coef_[0],huber.intercept_]))
